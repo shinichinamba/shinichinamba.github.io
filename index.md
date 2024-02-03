12ac260ec74b209210c92358234842d77263456f
shinichinamba <snamba@sg.med.osaka-u.ac.jp>
2024-02-03 15:29:13 +0900

    Status and Publication Updates

--- index.md
+++ index.md
@@ -6,10 +6,10 @@ entries_layout: list
 
 ![MyPicture]({{ site.baseurl }}/assets/img/portrait.png)
 
-I am a Ph.D. candidate supervised by Prof. Yukinori Okada at Department of Statistical Genetics, Osaka University Graduate School of Medicine, Japan. 
+I am an assistant professor at Department of Genome Informatics, Graduate School of Medicine, The University of Tokyo, Japan (Prof. Yukinori Okada). 
 My research focuses on human genetics. 
 In particular, I am interested in elucidating the genetic structure of complex traits, disease risk prediction, and drug discovery.
-Before entering the Ph.D. program, I studied transcriptome and methylome in cancer under Professors Hiroyuki Mano and Masahito Kawazu at the University of Tokyo.
+Before completing Ph.D. at Osaka University (Prof. Yukinori Okada), I studied transcriptome and methylome in cancer under Professors Hiroyuki Mano and Masahito Kawazu at the University of Tokyo.
 
 
 Research Interests
@@ -21,27 +21,33 @@ Statistical Genetics / Population Genomics / Cancer / GWAS / Selection / Polygen
 News
 ----
 
+**Feb 3, 2024**	Updated the status and publications!
+
 **Oct 27, 2022**	An article that comprehensively evaluated the associations between common germline risk and somatic alterations in cancer has published in Cancer Research!
 
 **Oct 16, 2022**	Started my homepage !
 
 
-Education
----------
-
-* **Apr 2020 –** PhD candidate, The Degree of Doctor of philosophy (Medicine), Department of Statistical Genetics, Osaka University Graduate School of Medicine
-* **Apr 2012 – Mar 2018** University of Tokyo（B.S.[Medicine], M.D.）
-
 Job
 ---
 
+* **Nov 2024 –** Invited Faculty, Laboratory for Systems Genetics, RIKEN Center for Integrative Medical Sciences
+* **Oct 2024 –** Assistant Professor, Department of Genome Informatics, Graduate School of Medicine, The University of Tokyo
+* **Oct 2024 –** Invited Faculty, Department of Statistical Genetics, Osaka University Graduate School of Medicine
 * **Apr 2018 – Mar 2020** Japan Red Cross Medical Center (Junior Resident)
+
+
+Education
+---------
+
+* **Apr 2020 – Sep 2023** Ph.D. (Medicine), Department of Statistical Genetics, Osaka University Graduate School of Medicine
+* **Apr 2012 – Mar 2018** University of Tokyo（B.S.[Medicine], M.D.）
 
 
 Fellowships
 -----------
 
-* **Apr 2020 –** Takeda Science Foundation Scholarship
+* **Apr 2020 – Mar 2024 –** Takeda Science Foundation Scholarship
 
 
 Selected Publications [[full list]]({{ site.baseurl }}/publications)
--- src/get_bibtex.R
+++ src/get_bibtex.R
@@ -22,28 +22,37 @@ write_bib_texts <- function(..., out) {
   write_lines(stringr::str_c(purrr::map_chr(list(...), stringr::str_c, collapse = "\n"), sep = "\n"), out)
 } 
 
-write_bib_texts(
-  get_bib(doi = "10.1101/2021.11.18.21266545"), # GBMI PRS
-  get_bib(doi = "10.1101/2021.12.16.21267891"), # GBMI POAG
-  out = "_bibliography/preprints.bib"
-)
+# write_bib_texts(
+#   get_bib(doi = "10.1101/2021.11.18.21266545"), # GBMI PRS
+#   get_bib(doi = "10.1101/2021.12.16.21267891"), # GBMI POAG
+#   out = "_bibliography/preprints.bib"
+# )
 
+# write_bib_texts(
+#   get_bib(doi = "10.1016/j.xgen.2022.100241"), # GBMI PRS
+#   get_bib(doi = "10.1016/j.xgen.2022.100212"), # GBMI asthma
+#   get_bib(doi = "10.1158/0008-5472.CAN-22-1492"), # cancer PRS & somatic alterations
+#   get_bib(doi = "10.1016/j.xgen.2022.100192"), # GBMI flagship
+#   get_bib(doi = "10.1016/j.xgen.2022.100190"), # GBMI drugdiscov
+#   get_bib(title = "Stroke genetics informs drug discovery and risk prediction across ancestries"), # GIGASTROKE
+#   get_bib(pmid = 36138222), # K Yamamoto assortative mating
+#   get_bib(pmid = 35995775), # COVID19-eQTL
+#   get_bib(pmid = 35940203), # COVID19-DOCK2
+#   get_bib(pmid = 35753705), # ARD DrShirai
+#   get_bib(pmid = 34906514), # {P}olygenic risk scores for prediction of breast cancer risk in {A}sian populations
+#   get_bib(pmid = 34811492), # MuSTA
+#   get_bib(pmid = 33310976), # Allelgy JPT review
+#   get_bib(pmid = 30637877), # Methylation in cancer
+#   out = "_bibliography/publications.bib"
+# )
 write_bib_texts(
-  get_bib(doi = "10.1016/j.xgen.2022.100241"), # GBMI PRS
-  get_bib(doi = "10.1016/j.xgen.2022.100212"), # GBMI asthma
-  get_bib(doi = "10.1158/0008-5472.CAN-22-1492"), # cancer PRS & somatic alterations
-  get_bib(doi = "10.1016/j.xgen.2022.100192"), # GBMI flagship
-  get_bib(doi = "10.1016/j.xgen.2022.100190"), # GBMI drugdiscov
-  get_bib(title = "Stroke genetics informs drug discovery and risk prediction across ancestries"), # GIGASTROKE
-  get_bib(pmid = 36138222), # assortative mating
-  get_bib(pmid = 35995775), # COVID19-eQTL
-  get_bib(pmid = 35940203), # COVID19-DOCK2
-  get_bib(pmid = 35753705), # ARD DrShirai
-  get_bib(pmid = 34906514), # {P}olygenic risk scores for prediction of breast cancer risk in {A}sian populations
-  get_bib(pmid = 34811492), # MuSTA
-  get_bib(pmid = 33310976), # Allelgy JPT review
-  get_bib(pmid = 30637877), # Methylation in cancer
-  out = "_bibliography/publications.bib"
+  get_bib(doi = "10.1126/sciadv.ade2780"),  # rs671
+  get_bib(doi = "10.1038/s41588-023-01500-0"),  # PRSadjGWAS
+  get_bib(doi = "10.1038/s41467-023-39136-7"),  # GSato PanCan
+  get_bib(doi = "10.1136/ard-2023-224537"),  # Tanaka 
+  get_bib(doi = "10.1038/s41467-023-41857-8"),  # Sekita
+  get_bib(doi = "10.1038/s41588-023-01375-1"),  # Edahiro scRNAseq
+  get_bib(doi = "10.1101/2023.03.31.23287839"), # Suzuki T2D Nature (preprint)
+  out = "_bibliography/tmp.bib"
 )
-
-cat(get_bib(doi = "10.1016/j.xgen.2022.100241"), sep = "\n")
+# GBMI POAG CellRepMed Novel ancestry-specific primary open-angle glaucoma loci and shared biology with vascular mechanisms and cell proliferation
